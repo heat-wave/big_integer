5b7e25274f3ef6897b8164333bf9e76910c05183
baba-beda <darya.nightmare@gmail.com>
2014-09-25 12:07:11 +0400

    it works without string and streams

--- big_integer.hpp
+++ big_integer.hpp
@@ -1,15 +1,17 @@
 #ifndef BIG_INTEGER_H
 #define BIG_INTEGER_H
 
-#include <gmp.h>
+//#include <gmp.h>
 #include <iosfwd>
+#include <vector>
 
 struct big_integer
 {
     big_integer();
+    big_integer(big_integer && other);
     big_integer(big_integer const& other);
-    big_integer(int a);
-    //explicit big_integer(std::string const& str);
+    big_integer(int a); // int has the same size as pointer
+   // explicit big_integer(std::string const& str);
     ~big_integer();
 
     big_integer& operator=(big_integer const& other);
@@ -17,6 +19,8 @@ struct big_integer
 
     big_integer& operator+=(big_integer const& rhs);
     big_integer& operator-=(big_integer const& rhs);
+
+    big_integer& operator*=(unsigned int rhs);
     big_integer& operator*=(big_integer const& rhs);
     //big_integer& operator/=(big_integer const& rhs);
     //big_integer& operator%=(big_integer const& rhs);
@@ -38,17 +42,18 @@ struct big_integer
     big_integer& operator--();
     big_integer operator--(int);
 
-    friend bool operator==(big_integer const& a, big_integer const& b);
-    friend bool operator!=(big_integer const& a, big_integer const& b);
-    friend bool operator<(big_integer const& a, big_integer const& b);
-    friend bool operator>(big_integer const& a, big_integer const& b);
-    friend bool operator<=(big_integer const& a, big_integer const& b);
-    friend bool operator>=(big_integer const& a, big_integer const& b);
+    bool operator==(big_integer const& b) const;
+    bool operator!=(big_integer const& b) const ;
+    bool operator<(big_integer const& b) const;
+    bool operator>(big_integer const& b) const;
+    bool operator<=(big_integer const& b) const;
+    bool operator>=(big_integer const& b) const;
 
     //friend std::string to_string(big_integer const& a);
+    //operator std::string() const;
 
 private:
-    vector<unsigned int> v;
+    std::vector<unsigned int> v;
     int sign;
 };
 
@@ -65,14 +70,16 @@ big_integer operator*(big_integer a, big_integer const& b);
 //big_integer operator<<(big_integer a, int b);
 //big_integer operator>>(big_integer a, int b);
 
-bool operator==(big_integer const& a, big_integer const& b);
+/*bool operator==(big_integer const& a, big_integer const& b);
 bool operator!=(big_integer const& a, big_integer const& b);
 bool operator<(big_integer const& a, big_integer const& b);
 bool operator>(big_integer const& a, big_integer const& b);
 bool operator<=(big_integer const& a, big_integer const& b);
-bool operator>=(big_integer const& a, big_integer const& b);
+bool operator>=(big_integer const& a, big_integer const& b);*/
 
-//std::string to_string(big_integer const& a);
-std::ostream& operator<<(std::ostream& s, big_integer const& a);
+
+/*std::ostream& operator<<(std::ostream& out, big_integer const& a);
+std::istream& operator>>(std::istream in, big_integer & a);*/
+
 
 #endif // BIG_INTEGER_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "big_integer.hpp"
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
+    big_integer a = 5;
+    big_integer b(10);
+    big_integer c = a + b;
+    c -= a;
+    c *= b;
   return 0;
 }
